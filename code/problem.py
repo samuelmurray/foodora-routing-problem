389b17395f636ba391855206f9235302301d9312
tarvedson <tilde.arvedson@gmail.com>
2016-10-06 18:13:56 +0200

    Added make_pddl in problem

--- code/problem.py
+++ code/problem.py
@@ -3,7 +3,7 @@ from graph import graph
 
 
 class Problem:
-	def __init__(self,graph: graph, init_state: dict, goal_state: dict, bikers: list, restaurants: list, customer_orders: list):
+	def __init__(self.graph: graph, init_state: dict, goal_state: dict, bikers: list, restaurants: list, customer_orders: list):
 		self.graph = graph
 		self.init_state = init_state
 		self.goal_state = goal_state
@@ -29,6 +29,43 @@ class Problem:
 
 	def get_customer_orders():
 		return self.customer_orders
+
+	def make_pddl():
+		with open('pddl_init.ppdl', 'w') as out:
+		    out.write('(define (problem ppdl_init)\n (:domain foodora_domain)\n (:objects ')
+		    for c in self.customer_orders.keys():
+		        out.write(c + ' ')
+		    out.write('- customer\n')
+		    for r in self.restaurants:
+		        out.write(r + ' ')
+		    out.write('- restaurant\n')
+		    for b in self.bikers:
+		        out.write(b + ' ')
+		    out.write('- biker\n')
+		    for n in self.graph.nodes():
+		        out.write(n.name() + ' ')
+		        out.write('- node)\n')
+
+		    out.write('(:init\n')
+
+		    #Improve when I understand structure on init_state
+
+		    for c in self.customer_orders.keys():
+				out.write('at-c ' + c)
+		    for r in self.restaurants:
+				out.write('at-r ' + r)
+		    for e in self.graph.edges():
+		    	out.write('edge' + e[0] + ' ' + e[1] + '\n')
+
+		    #out.write('rGotFoodFor ')
+		    #out.write('notHavFood ')
+
+		    out.write('(:goal (and')
+		    for c in self.customer_orders.keys():
+				out.write(' (gotFood ' + c + ')')
+			out.write('))\n)')
+
+
 
 
 
